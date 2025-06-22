1fa438178b1b987a42f76b78dae0b584a58f02c1
johhnynouz <johhnynouz@yandex.ru>
2025-06-22 18:09:14 +0300

    добавлена верстка второстепенных страниц3.0

--- src/app/ranenie/page.tsx
+++ src/app/ranenie/page.tsx
@@ -16,6 +16,23 @@ export default function Home(){
                     <AskQuestion/>
                 </div>
             </div>
+            <div className="main">
+                <h2 className="main__header">С нами работают военнослужащие:</h2>
+                <div className="main__list">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="main__checklist" src="/img/mo_rf.jpg" alt="mo_rf"/>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="main__checklist" src="/img/fsbrf.jpg" alt="fsb"/>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="main__checklist" src="/img/sk_rf.jpg" alt="sk"/>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="main__checklist" src="/img/mcs_rf.jpg" alt="mcs"/>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="main__checklist" src="/img/mvd_rf.jpg" alt="mvd"/>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="main__checklist" src="/img/rosgv.jpg" alt="rosgv"/>
+                </div>
+            </div>
             <div className="top__ranenie">
                 <div className="ranenie">
                     {/*<h1></h1><br/>*/}
